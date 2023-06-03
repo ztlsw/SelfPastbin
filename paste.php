74afc3b4a0ddc7ffaef3ea1d61fc7936ea628de8
ztlsw <752667144@qq.com>
2023-06-03 09:34:16 +0800

    used self css

--- paste.php
+++ paste.php
@@ -1,5 +1,5 @@
 <?php
-
+ob_start();//调试用
 function uuid() {  
     $chars = md5(uniqid(mt_rand(), true));  
     $uuid = substr ( $chars, 0, 8 ) . '-'
@@ -7,14 +7,17 @@ function uuid() {
             . substr ( $chars, 12, 4 ) . '-'
             . substr ( $chars, 16, 4 ) . '-'
             . substr ( $chars, 20, 12 );  
-    return $uuid ;  
+    return $uuid ;
 }  
 
-var_dump($_POST);
+var_dump($_POST);    //调试用
 $filename = uuid();
-
-file_put_contents("codes\\title\\" . $filename . '.txt', $_POST['title']);
-file_put_contents("codes\\content\\" . $filename . '.txt', htmlspecialchars($_POST['content']));
+$file_path1 = 'codes' . DIRECTORY_SEPARATOR . 'title' . DIRECTORY_SEPARATOR . $filename . '.txt';
+$file_path2 = 'codes' . DIRECTORY_SEPARATOR . 'content' . DIRECTORY_SEPARATOR . $filename . '.txt';
+file_put_contents($file_path1, $_POST['title']);
+file_put_contents($file_path2, htmlspecialchars($_POST['content']));
 
 header("location:view.php?id=".$filename);
+
+ob_end_flush();  //调试用
 ?>
--- view.php
+++ view.php
@@ -1,18 +1,17 @@
 <?php
     $id = $_GET['id'];
     $filename = $id . '.txt';
-    $title = file_get_contents("codes\\title\\" . $filename);
-    $content = file_get_contents("codes\\content\\" . $filename);
+    $title = file_get_contents("codes" . DIRECTORY_SEPARATOR . "title" . DIRECTORY_SEPARATOR . $filename);
+    $content = file_get_contents("codes" . DIRECTORY_SEPARATOR . "content" . DIRECTORY_SEPARATOR . $filename);
+
 ?>
 <!DOCTYPE html>
-<html lang="zh-cn">
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Paste Code</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-7ymO4nGrkm372HoSbq1OY2DP4pEZnMiA+E0F3zPr+JQQtQ82gQ1HPY3QIVtztVua" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/atom-one-dark.min.css">
-    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
+    <link rel="stylesheet" href="style.css" type = "text/css"/>
+    <script src="script.js"></script>
 </head>
 <body>
     <div class="container" style="padding-top:30px;">
